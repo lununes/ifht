e8be03668d5c20bd8b7c80d79914ee8120be14a1
lununes <luciana.mn@gmail.com>
2020-10-02 14:09:28 -0300

    Inserção do podcast

---
+++ recursos-visuais/js/podcast.js
@@ -0,0 +1,20 @@
+//Para iniciar o Podcast na página
+document.addEventListener('DOMContentLoaded', function() {
+    GreenAudioPlayer.init({
+        selector: '.player',
+        stopOthersOnPlay: true
+    });
+  
+    GreenAudioPlayer.init({
+        selector: '.player-with-download',
+        stopOthersOnPlay: true,
+        showDownloadButton: true,
+        enableKeystrokes: true
+    });
+  
+    GreenAudioPlayer.init({
+        selector: '.player-with-accessibility',
+        stopOthersOnPlay: true,
+        enableKeystrokes: true
+    });
+  });
